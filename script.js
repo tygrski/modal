8e7c27a63ba73d13a9719edf666ed77b8a0073ed
tygrski <77369211+tygrski@users.noreply.github.com>
2022-08-24 19:51:14 -0500

    init commit/ basic modal functions

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const openModal = function (){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+// loop through modal open buttins
+for(let i = 0; i < btnOpenModal.length ; i++)
+    btnOpenModal[i].addEventListener('click', openModal);
+
+// close modal 
+btnCloseModal.addEventListener('click', closeModal);
+
+// clicking outside modal/on overlay closes modal
+overlay.addEventListener('click', closeModal);
+
