1a024c143bfd30f1e4a5c8f4a165d82a941abdfc
tygrski <77369211+tygrski@users.noreply.github.com>
2022-08-24 20:07:49 -0500

    esc key functino on modal

--- script.js
+++ script.js
@@ -4,22 +4,29 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-const openModal = function (){
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
-const closeModal = function(){
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
 // loop through modal open buttins
-for(let i = 0; i < btnOpenModal.length ; i++)
-    btnOpenModal[i].addEventListener('click', openModal);
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', openModal);
 
-// close modal 
+// close modal
 btnCloseModal.addEventListener('click', closeModal);
 
 // clicking outside modal/on overlay closes modal
 overlay.addEventListener('click', closeModal);
 
+// close modal by 'esc' key event
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
